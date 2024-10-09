6f072e0dec1c357eea8ca7a76760706e4d4f12f5
vikramsamak <vikramsamak02@gmail.com>
2024-10-09 23:48:56 +0530

    updated as per new feature release.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,27 +1,71 @@
 import { useState } from "react";
-import { FormatType, RealTimeClock, TimezoneType } from "react-realtime-clock";
+import {
+  ClockType,
+  FormatType,
+  RealTimeClock,
+  TimezoneType,
+} from "react-realtime-clock";
 import { timeZones } from "./constants/Timezones";
 import { formats } from "./constants/Formats";
+import { startCase } from "lodash";
+import "react-clock/dist/Clock.css";
+import {
+  ANALOG_CLOCK_TYPE,
+  DEFAULT_TIME_FORMAT,
+  DIGITAL_CLOCK_TYPE,
+  INDIAN_TIME_ZONE,
+} from "./constants/Constants";
 
 function App() {
   const [selectedTimeZone, setSelectedTimeZone] =
-    useState<TimezoneType>("Asia/Kolkata");
+    useState<TimezoneType>(INDIAN_TIME_ZONE);
   const [selectedFormat, setSelectedFormat] =
-    useState<FormatType>("hh:mm:ss A");
+    useState<FormatType>(DEFAULT_TIME_FORMAT);
+  const [selectedClockType, setSelectedType] =
+    useState<ClockType>(DIGITAL_CLOCK_TYPE);
 
   return (
-    <main className="min-h-screen flex flex-col justify-center items-center bg-black text-white p-4">
-      <h1 className="text-4xl font-bold mb-6">Real Time Clock</h1>
-
-      <section className="max-w-screen-md flex flex-col gap-4">
-        <RealTimeClock
-          clockTextClassName="font-extrabold  text-4xl text-white"
-          containerClassName="bg-tranparent w-full"
-          timeZone={selectedTimeZone}
-          format={selectedFormat}
-        />
-        <div className="flex flex-col  gap-4 w-full px-2">
-          <div className="w-full">
+    <main className="min-h-screen flex flex-col bg-black text-white p-4 gap-12">
+      <h1 className="text-4xl font-bold py-5 w-full text-center">
+        React Real Time Clock
+      </h1>
+      <section className="flex flex-col gap-4 flex-grow">
+        <div className="flex flex-grow w-full justify-center items-center">
+          <RealTimeClock
+            clockTextClassName="font-extrabold text-4xl text-white"
+            containerClassName="w-full"
+            analogClockClassName="rounded-full"
+            timeZone={selectedTimeZone}
+            format={selectedFormat}
+            clockType={selectedClockType}
+            clockSize={200}
+            renderAnalogClockNumbers={true}
+          />
+        </div>
+        <div className="flex flex-col md:flex-row gap-4 p-4 w-full justify-between">
+          <div
+            className={`${
+              selectedClockType === DIGITAL_CLOCK_TYPE ? "md:w-1/3" : "W-1/2"
+            } w-full `}
+          >
+            <label className="text-lg">Select Clock Type:</label>
+            <select
+              value={selectedClockType}
+              onChange={(e) => setSelectedType(e.target.value as ClockType)}
+              className="p-2 bg-gray-800 border border-gray-600 rounded-lg w-full"
+            >
+              {[DIGITAL_CLOCK_TYPE, ANALOG_CLOCK_TYPE].map(
+                (type: string, i: number) => (
+                  <option key={i} value={type}>
+                    {startCase(type.toUpperCase())}
+                  </option>
+                )
+              )}
+            </select>
+          </div>
+          <div className={`${
+              selectedClockType === DIGITAL_CLOCK_TYPE ? "md:w-1/3" : "W-1/2"
+            } w-full `}>
             <label className="text-lg">Select Time Zone:</label>
             <input
               list="timezones"
@@ -33,27 +77,31 @@ function App() {
               placeholder="Select Timezone"
             />
             <datalist id="timezones">
-              {timeZones.map((zone) => (
-                <option key={zone} value={zone}>
+              {timeZones.map((zone: string, i: number) => (
+                <option key={i} value={zone}>
                   {zone}
                 </option>
               ))}
             </datalist>
           </div>
-          <div className="w-full">
-            <label className="text-lg">Select Format:</label>
-            <select
-              value={selectedFormat}
-              onChange={(e) => setSelectedFormat(e.target.value as FormatType)}
-              className="p-2 bg-gray-800 border border-gray-600 rounded-lg w-full"
-            >
-              {formats.map((format) => (
-                <option key={format} value={format}>
-                  {format}
-                </option>
-              ))}
-            </select>
-          </div>
+          {selectedClockType === DIGITAL_CLOCK_TYPE && (
+            <div className="w-full md:w-1/3">
+              <label className="text-lg">Select Format:</label>
+              <select
+                value={selectedFormat}
+                onChange={(e) =>
+                  setSelectedFormat(e.target.value as FormatType)
+                }
+                className="p-2 bg-gray-800 border border-gray-600 rounded-lg w-full"
+              >
+                {formats.map((format) => (
+                  <option key={format} value={format}>
+                    {format}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
         </div>
       </section>
     </main>
---
+++ example/src/constants/Constants.ts
@@ -0,0 +1,4 @@
+export const DIGITAL_CLOCK_TYPE = "digital";
+export const ANALOG_CLOCK_TYPE = "analog";
+export const INDIAN_TIME_ZONE = "Asia/Kolkata";
+export const DEFAULT_TIME_FORMAT= "hh:mm:ss A";
