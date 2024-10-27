d596d3bb14fad368a3e9a6512f5f572faf072209
vikramsamak <vikramsamak02@gmail.com>
2024-10-27 11:15:40 +0530

    Added more hooks.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+//COMPONENTS
 export { default as RealTimeClock } from "./components/RealtimeClock";
+//HOOKS
 export { default as useTimeElapsed } from "./hooks/useTimeElapsed";
 export { default as useNotificationAlarm } from "./hooks/useNotificationAlarm";
+export { default as useStopWatch } from "./hooks/useStopWatch";
+export { default as useCountDown } from "./hooks/useCountDown";
+//TYPES
 export type { FormatType } from "./types/FormatType";
 export type { TimezoneType } from "./types/TimezoneType";
 export type { ClockType } from "./types/Types";
